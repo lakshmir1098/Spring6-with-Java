131a6a98bfcf895012c51b1e133c19f5850b5c64
lramachandran <Lakshmi.Ramachandran@zyter.com>
2024-12-30 14:51:26 +0530

    LC 1

---
+++ working/LooseCoupling/learningSpring/src/main/java/com/lakshmilearning/learningSpring/GamingConsole.java
@@ -0,0 +1,16 @@
+package com.lakshmilearning.learningSpring;
+
+import com.lakshmilearning.learningSpring.game.Game01;
+import com.lakshmilearning.learningSpring.game.Game02;
+import com.lakshmilearning.learningSpring.game.GameRunner;
+
+public class GamingConsole {
+    public static void main(String[] args) {
+        var g1 = new Game01();
+        var g2 = new Game02();
+        var gameRunner = new GameRunner(g2);
+
+        gameRunner.run();
+        g2.jump();
+    }
+}
